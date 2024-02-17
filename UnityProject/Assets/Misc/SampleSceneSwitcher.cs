e390f7c138aa8ef4c62e9f1caf2802dc75a7d8a7
Steve Vermeulen <sfvermeulen@gmail.com>
2024-02-17 21:41:13 +0800

    Fixes for webgl build

---
+++ UnityProject/Assets/Misc/SampleSceneSwitcher.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Linq;
+
+namespace Zenject
+{
+    public class SampleSceneSwitcher : MonoBehaviour
+    {
+        static readonly string[] SceneNames = new string[]
+        {
+            "Asteroids",
+            "SpaceFighter",
+        };
+
+        public void Update()
+        {
+            // This is just used to make it easier to test builds
+            if (Input.GetKeyDown(KeyCode.Space) && Input.GetKey(KeyCode.LeftControl))
+            {
+                var currentSceneName = SceneManager.GetActiveScene().name;
+                var otherSceneName = SceneNames.First(x => x != currentSceneName);
+
+                SceneManager.LoadScene(otherSceneName);
+            }
+        }
+    }
+}
